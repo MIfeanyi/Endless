1a0dafdc7d45887bf579b99ee2cf3c840768285f
Michael Redford <michaeliredford@gmail.com>
2017-09-25 16:32:03 -0600

    updated tileset collisionboxes

--- tutorial.gd
+++ tutorial.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 onready var anim = get_node("anim")
-onready var label = get_node("label")
+onready var label = get_node("Label")
 func _ready():
 	print(self.get_path())
 	pass
 
 func display(param1):
-	get_node("Label").set_text(param1)
-	get_node("anim").play("Fade in")
+	label.set_text(param1)
+	anim.play("Fade in")
 	pass
 func done():
 	if !anim.is_playing():
